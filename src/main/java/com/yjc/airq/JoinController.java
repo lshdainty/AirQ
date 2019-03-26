3d540b2b51faf586b59c54de87bf054577271338
lshdainty <45247068+lshdainty@users.noreply.github.com>
2019-03-26 20:08:47 +0900

    2019/03/26 경로설정 초기

--- src/main/java/com/yjc/airq/JoinController.java
+++ src/main/java/com/yjc/airq/JoinController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import lombok.AllArgsConstructor;
 
 /**
- * home화면을 관리하는 controller
+ * 회원가입을 관리하는 controller
  */
 @Controller
 @AllArgsConstructor
-public class HomeController {
-
-	//홈 메인페이지로 가기
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Model model) {
-		return "home";
+public class JoinController {
+	
+	//회원가입 메인페이지로 가기
+	@RequestMapping(value = "/joinMain", method = RequestMethod.GET)
+	public String joinMain(Model model) {
+		return "join/joinMain";
 	}
 	
 }
