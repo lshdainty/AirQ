70df32b66b1ecd6fbc192e41ec8221c4acd78092
Baesuhwi <44791399+Baesuhwi@users.noreply.github.com>
2019-03-29 09:27:36 +0900

    2019/03/29 회원가입3

--- src/main/java/com/yjc/airq/mapper/MemberMapper.java
+++ src/main/java/com/yjc/airq/mapper/MemberMapper.java
@@ -1,16 +1,13 @@
 package com.yjc.airq.mapper;
 
 import java.util.ArrayList;
-
 import org.apache.ibatis.annotations.Param;
-
 import com.yjc.airq.domain.FileTestVO;
 import com.yjc.airq.domain.SellerVO;
 import com.yjc.airq.domain.MemberVO;
 
 public interface MemberMapper {
 	public ArrayList<MemberVO> getMemberList();
-<<<<<<< HEAD
 
 	public MemberVO login(@Param("id") String id, @Param("password") String password);
 
@@ -28,9 +25,4 @@ public interface MemberMapper {
 
 	// 파일 업로드
 	public String fileDB(FileTestVO file);
-=======
-	public MemberVO login(String id);
-	public MemberVO findId(MemberVO LVOI);
-	public MemberVO findPw(MemberVO LVOP);
->>>>>>> 80e7843e99ba3f83a9f08d0e188901833fe3f914
 }
--- src/main/java/com/yjc/airq/service/MemberService.java
+++ src/main/java/com/yjc/airq/service/MemberService.java
@@ -6,7 +6,5 @@ import com.yjc.airq.domain.MemberVO;
 
 public interface MemberService {
 	public ArrayList<MemberVO> getMemberList();
-	public MemberVO login(String id);
-	public MemberVO findId(MemberVO LVOI);
-	public MemberVO findPw(MemberVO LVOP);
+	public MemberVO login(String id, String password);
 }
