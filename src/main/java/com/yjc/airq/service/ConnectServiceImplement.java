5fd9fd939c083e641879479487d2b25de67f6551
lshdainty <45247068+lshdainty@users.noreply.github.com>
2019-03-30 14:19:11 +0900

    2019/03/30 상품리스트 복구완료

--- src/main/java/com/yjc/airq/service/ConnectServiceImplement.java
+++ src/main/java/com/yjc/airq/service/ConnectServiceImplement.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
+import com.yjc.airq.domain.ProductVO;
 import com.yjc.airq.domain.TenderboardVO;
+import com.yjc.airq.mapper.ProductMapper;
 import com.yjc.airq.mapper.TenderboardMapper;
 
 import lombok.AllArgsConstructor;
@@ -12,11 +14,18 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class ConnectServiceImplement implements ConnectService {
-	private TenderboardMapper mapper;
+	private TenderboardMapper Tendermapper;
+	private ProductMapper productMapper;
 	
 	// 입찰 리스트 출력
 	@Override
 	public ArrayList<TenderboardVO> tenderList() {
-		return mapper.tenderList();
+		return Tendermapper.tenderList();
 	}
+	
+	// 상품 리스트 출력
+	@Override
+	public ArrayList<ProductVO> productList(){
+		return productMapper.productList();
+	};
 }
