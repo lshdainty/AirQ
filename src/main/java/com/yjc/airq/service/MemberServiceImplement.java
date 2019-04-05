1d0b0c49d10a0242cd4ab3fae80b579592c6cd22
CheonYohan <kero74@naver.com>
2019-04-05 22:15:27 +0900

    2019/04/05 post

--- src/main/java/com/yjc/airq/service/MemberServiceImplement.java
+++ src/main/java/com/yjc/airq/service/MemberServiceImplement.java
@@ -18,20 +18,23 @@ public class MemberServiceImplement implements MemberService{
 	public ArrayList<MemberVO> getMemberList(){
 		return mapper.getMemberList();
 	}
-	
-	@Override
-	public MemberVO findId(MemberVO LVOI) {
-		return mapper.findId(LVOI);
-	}
-
-	@Override
-	public MemberVO findPw(MemberVO LVOP) {
-		return mapper.findPw(LVOP);
-	}
 
 	@Override
 	public MemberVO login(String id) {
 		// TODO Auto-generated method stub
 		return mapper.login(id);
+	}
+
+	@Override
+	public MemberVO findPw(String name, String id, String tel, String email) {
+		// TODO Auto-generated method stub
+		
+		return mapper.findPw(name,id,tel,email);
+	}
+
+	@Override
+	public MemberVO findId(String name, String tel, String email) {
+		// TODO Auto-generated method stub
+		return mapper.findId(name,tel,email);
 	}
 }
