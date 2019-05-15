1ea39ca4d4a0434d5b9afec2327ab87737376890
CheonYohan <kero74@naver.com>
2019-05-15 17:52:27 +0900

    app

--- app/airq/www/resources/js/login/loginMain.js
+++ app/airq/www/resources/js/login/loginMain.js
@@ -1,24 +1,84 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+	$('#memberInput2562').focus(function () {
+		$('#memberInput2562').parent().addClass("member-input__state--focus");
+	});
+	$('#memberInput2562').focusout(function () {
+		$('#memberInput2562').parent().removeClass("member-input__state--focus");
+	});
+
+	$('#memberInput2562').keyup(function () {
+		var input = $('#memberInput2562');
+		var value = input.val();
+		if (value!=""){
+			$('#memberInput2562').parent().addClass("member-input__state--value");
+			if($('#memberInput797').val() != "")
+				$('.login__btn').attr("disabled",false);
+			else
+				$('.login__btn').attr("disabled",true);
+		}
+		else{
+			$('#memberInput2562').parent().removeClass("member-input__state--value");
+			$('.login__btn').attr("disabled",true);
+		}
+	});
+
+	$('#memberInput797').focus(function () {
+		$('#memberInput797').parent().addClass("member-input__state--focus");
+	});
+	$('#memberInput797').focusout(function () {
+		$('#memberInput797').parent().removeClass("member-input__state--focus");
+	});
+
+	$('#memberInput797').keydown(function () {
+		if ($('#memberInput797').val() != ""){
+			$('#memberInput797').parent().addClass("member-input__state--value");
+			if($('#memberInput797').val() != "")
+			$('.login__btn').attr("disabled",false);
+		else
+			$('.login__btn').attr("disabled",true);
+		}
+		else{
+			$('#memberInput797').parent().removeClass("member-input__state--value");
+			$('.login__btn').attr("disabled",true);
+		}
+	});
+	var isChecked = false;
+	$(".member-checkbox__state").click(function () {
+		if (isChecked==false){
+			$(".member-checkbox__state").addClass("member-checkbox__state--checked");
+			isChecked=true;
+		}
+		else{
+			$(".member-checkbox__state").removeClass("member-checkbox__state--checked");
+			isChecked=false;
+		}
+	});
+
+	$('#memberInput2562').val() != null && $('#memberInput797').val() !={
+
+	}
+
 	//로그인 버튼 클릭
-	$("#logintest").click(function() {
-		
+	$("#logintest").click(function () {
+
 		var query = {
-			id : $("#id").val(),
-			password : $("#pw").val()
+			id: $("#id").val(),
+			password: $("#pw").val()
 		}
 		// 로그인 버튼 클릭 ajax 
 		$.ajax({
-			type : "POST",
-			data : query,
-			url : "login", // 로그인 페이지 경로
-			success : function(data) {
+			type: "POST",
+			data: query,
+			url: "login", // 로그인 페이지 경로
+			success: function (data) {
 				if (data == "success") { //로그인 성공
 					location.href = "/";
-				} else if(data == "failpw"){ //pw 틀렸을 경우
+				} else if (data == "failpw") { //pw 틀렸을 경우
 					$("#nopw").css("display", "block");
 					$("#nopw").show();
 					$("#noid").hide();
-				}else if(data == "failid"){ //id 틀렸을 경우
+				} else if (data == "failid") { //id 틀렸을 경우
 					$("#noid").css("display", "blokc");
 					$("#noid").show();
 					$("#nopw").hide();
@@ -26,18 +86,18 @@ $(document).ready(function() {
 			}
 		}) // ajax 로그인 버튼 끝
 	});
-	   //패스워드에 커서를 두고 엔터키를 누르면 로그인 함
-	   $("#pw").keydown(function(key) {
-	      if (key.keyCode == 13) {
-	         $("#logintest").click();
-	      }
-	   });
-	   
-	   //아이디에 커서를 두고 엔터키를 누르면 로그인 함
-	   $("#id").keydown(function(key) {
-	      if (key.keyCode == 13) {
-		     $("#logintest").click();
-	      }
-	   });
-	
+	//패스워드에 커서를 두고 엔터키를 누르면 로그인 함
+	$("#pw").keydown(function (key) {
+		if (key.keyCode == 13) {
+			$("#logintest").click();
+		}
+	});
+
+	//아이디에 커서를 두고 엔터키를 누르면 로그인 함
+	$("#id").keydown(function (key) {
+		if (key.keyCode == 13) {
+			$("#logintest").click();
+		}
+	});
+
 });
