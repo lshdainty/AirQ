8301ea8005b841e3ce70b5c35d508424972ddb25
lshdainty <45247068+lshdainty@users.noreply.github.com>
2019-05-15 18:54:46 +0900

    2019/05/15 미세먼지 수치 표시

--- web/src/main/webapp/resources/js/manage/monitoringMain.js
+++ web/src/main/webapp/resources/js/manage/monitoringMain.js
@@ -1,7 +1,20 @@
+var test;
+
+$.ajax({
+	type: "get",
+	url: "/test1111",
+	dataType:"json",
+	async: false,
+	success: function(data) {
+		console.log(data);
+		test = data;
+	}
+});
+
 // 네이버 지도 자바 스크립트 시작
 var mapOptions = {
-    center: new naver.maps.LatLng(33.450705, 126.570677),
-    zoom: 10,
+    center: new naver.maps.LatLng(35.8967746, 128.6209546),
+    zoom: 2,
     scaleControl: false,
     logoControl: false,
     mapDataControl: false,
@@ -13,94 +26,72 @@ var map = new naver.maps.Map('map', mapOptions);
 
 //2. HTML 마커
 //JSON 데이터부분
-var marker_position = {
-	"positions": [
-		{
-			"title":"카카오",
-		    "lat": 33.450705,
-		    "lng": 126.570677
-		 },
-		 {
-		    "title":"생태연못",
-		    "lat": 33.450936,
-		    "lng": 126.569477
-		 },
-		 {
-			 "title":"텃밭",
-		     "lat": 33.450879,
-		     "lng": 126.569940
-		 },
-		 {
-		     "title":"근린공원",
-		     "lat": 33.451393,
-		     "lng": 126.570738
-		 }
-	]
-}
 
-var htmlMarkers = [];
+var marker_position = test;
 
-var htmlMarker1 = {
-        content: '<div id="htmlMarker1"></div>',
-        size: N.Size(40, 40),
-        anchor: N.Point(20, 20)
-    },
-    htmlMarker2 = {
-        content: '<div id="htmlMarker2"></div>',
-        size: N.Size(40, 40),
-        anchor: N.Point(20, 20)
-    },
-    htmlMarker3 = {
-        content: '<div id="htmlMarker3"></div>',
-        size: N.Size(40, 40),
-        anchor: N.Point(20, 20)
-    },
-    htmlMarker4 = {
-        content: '<div id="htmlMarker4"></div>',
-        size: N.Size(40, 40),
-        anchor: N.Point(20, 20)
-    },
-    htmlMarker5 = {
-        content: '<div id="htmlMarker5"></div>',
-        size: N.Size(40, 40),
-        anchor: N.Point(20, 20)
-    };
+	var htmlMarkers = [];
 
-//마커찍는 부분
-for(var i=0; i<marker_position.positions.length; i++){
-	
-	//html기반 마커찍기
-	var markerContent = [
-        '<div style="position:absolute;">',
-            '<div class="infowindow">',
-                '<div>이산화탄소 : '+marker_position.positions[i].title+'</div>',
-                '<div class="info_tri1"></div>',
-                '<div class="info_tri2"></div>',
-            '</div>',
-            '<div class="pin_s">',
-                '<img class="pin_s_img" src="https://ssl.pstatic.net/static/maps/mantle/1x/marker-default.png" alt="">',
-                '<div class="pins_s_tooltip">'+marker_position.positions[i].title+'</div>',
-            '</div>',
-        '</div>'
-    ].join('');
-	
-	//json에 저장된 위치정보 하나씩 가져오기
-	var position = new naver.maps.LatLng(
-			marker_position.positions[i].lat,
-			marker_position.positions[i].lng);
-	
-	//html마커 naver map에 생성
-	var htmlMarker = new naver.maps.Marker({
-        position: position,
-        map: map,
-        icon: {
-            content: markerContent,
-            size: new naver.maps.Size(22, 30),
-            anchor: new naver.maps.Point(11, 30)
-        }
-    });
-	htmlMarkers.push(htmlMarker);
-}
+	var htmlMarker1 = {
+	        content: '<div id="htmlMarker1"></div>',
+	        size: N.Size(40, 40),
+	        anchor: N.Point(20, 20)
+	    },
+	    htmlMarker2 = {
+	        content: '<div id="htmlMarker2"></div>',
+	        size: N.Size(40, 40),
+	        anchor: N.Point(20, 20)
+	    },
+	    htmlMarker3 = {
+	        content: '<div id="htmlMarker3"></div>',
+	        size: N.Size(40, 40),
+	        anchor: N.Point(20, 20)
+	    },
+	    htmlMarker4 = {
+	        content: '<div id="htmlMarker4"></div>',
+	        size: N.Size(40, 40),
+	        anchor: N.Point(20, 20)
+	    },
+	    htmlMarker5 = {
+	        content: '<div id="htmlMarker5"></div>',
+	        size: N.Size(40, 40),
+	        anchor: N.Point(20, 20)
+	    };
+
+	//마커찍는 부분
+	for(var i=0; i<marker_position.result.length; i++){
+		
+		//html기반 마커찍기
+		var markerContent = [
+	        '<div style="position:absolute;">',
+	            '<div class="infowindow">',
+	                '<div>미세먼지(pm10) : '+marker_position.result[i].pm10Value+'</div>',
+	                '<div class="info_tri1"></div>',
+	                '<div class="info_tri2"></div>',
+	            '</div>',
+	            '<div class="pin_s">',
+	                '<img class="pin_s_img" src="https://ssl.pstatic.net/static/maps/mantle/1x/marker-default.png" alt="">',
+	                '<div class="pins_s_tooltip">'+marker_position.result[i].addr+'</div>',
+	            '</div>',
+	        '</div>'
+	    ].join('');
+		
+		//json에 저장된 위치정보 하나씩 가져오기
+		var position = new naver.maps.LatLng(
+				marker_position.result[i].x,
+				marker_position.result[i].y);
+		
+		//html마커 naver map에 생성
+		var htmlMarker = new naver.maps.Marker({
+	        position: position,
+	        map: map,
+	        icon: {
+	            content: markerContent,
+	            size: new naver.maps.Size(22, 30),
+	            anchor: new naver.maps.Point(11, 30)
+	        }
+	    });
+		htmlMarkers.push(htmlMarker);
+	}
 
 //마커 클러스터 생성
 var markerClustering = new MarkerClustering({
