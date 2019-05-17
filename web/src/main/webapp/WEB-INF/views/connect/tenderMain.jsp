66166d6c9bc6b2c1c6ec4000153bc3462d3757e7
euuun2 <zmxn4235@naver.com>
2019-05-17 19:08:30 +0900

    2019.05.17

--- web/src/main/webapp/WEB-INF/views/connect/tenderMain.jsp
+++ web/src/main/webapp/WEB-INF/views/connect/tenderMain.jsp
@@ -11,15 +11,6 @@
 	<div id="tenderContainer">
 		<h1 id="tenderTitle" class="tenderTitle">입찰 공고</h1>
 		<div id="tenderSelectDiv" class="tenderSelectDiv">
-			<select class="tenderSelect">
-				<option>전체 글 보기</option>
-				<option>내가 쓴 글 보기</option>
-			</select> 
-			<select class="tenderSelect">
-				<option>날짜</option>
-				<option>참여업체수</option>
-				<option>마감기한</option>
-			</select>
 		</div>
 		<ul class="tableList">
 			<li class="tableListHeader">
@@ -32,7 +23,7 @@
 			</li>
 			<c:forEach var="tenderList" items="${tenderList}">
 				<li class="tableListContent tenderLiContent post-item" id="${tenderList.tender_code }">
-					<div class="tableColumn tableCol-10-1" data-label="번호">${tenderList.rownum }</div>
+					<div class="tableColumn tableCol-10-1" data-label="번호">d${tenderList.rnum }</div>
 					<div class="tableColumn tableCol-30" data-label="제목">${tenderList.tender_title }</div>
 					<div class="tableColumn tableCol-15" data-label="글쓴이">${tenderList.member_id }</div>
 					<div class="tableColumn tableCol-15" data-label="등록일">${tenderList.t_creation_date }</div>
@@ -41,11 +32,23 @@
 				</li>
 			</c:forEach>
 		</ul>
-		<c:if test="${sessionScope.user.member_devision == 'no' }">
-			<div id="tenderWriteDiv" class="tenderWriteDiv">
-				<input type="submit" id="tenderWrite" class="tenderWriteBtn" name="tenderWrite" value="작성하기">
-			</div>
-		</c:if>
+		<div id="tenderWriteDiv" class="tenderWriteDiv">
+		</div>
+		<nav aria-label="Page navigation example">
+				<div class="d-flex justify-content-center">
+					<ul class="pagination">
+						<c:if test="${criteria.prev}">
+							<li class="page-item"><a class="page-link" href="javascript:page(${criteria.getStartPage()-1});" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>
+						</c:if>
+						<c:forEach begin="${criteria.getStartPage() }" end="${criteria.getEndPage() }" var="idx">
+							<li class="page-item"><a class="page-link" href="javascript:page(${idx });">${idx}</a></li>
+						</c:forEach>
+						<c:if test="${criteria.next}">
+							<li class="page-item"><a class="page-link" href="javascript:page(${criteria.getEndPage()+1});" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>
+						</c:if>
+					</ul>
+				</div>
+			</nav>
 	</div>
 </form>
 <%--입찰 공고 테이블 끝 --%>
--- web/src/main/webapp/resources/js/connect/tenderMain.js
+++ web/src/main/webapp/resources/js/connect/tenderMain.js
@@ -1,3 +1,80 @@
+function sortdata(){
+	var sort=$(".tenderSelect option:selected").val();
+	return sort;
+}
+
+$("#tenderSelectDiv").change(function(){
+	alert("dd");
+	ajax(1,sortdata());
+});
+
+function page(idx){
+	alert(idx);
+	ajax(idx,sortdata());
+}
+function ajax(idx,sort){
+	var data={
+			sort:sort
+	};
+	
+	$.ajax({
+		type:"post",
+		url:"/selectTender?pagenum="+idx,
+		data:data,
+		async:false,
+		dataType:"json",
+		success:function(data){
+			console.log(data);
+			for(var i=0;i<data.tenderList.length;i++){
+				var tenderList='<li class="tableListContent tenderLiContent post-item" id="'+data.tenderList[i].tender_code+'">'
+					+'<div class="tableColumn tableCol-10-1" data-label="번호">'+data.tenderList[i].rnum+'</div>'
+					+'<div class="tableColumn tableCol-30" data-label="제목">'+data.tenderList[i].tender_title+'</div>'
+					+'<div class="tableColumn tableCol-15" data-label="글쓴이">'+data.tenderList[i].member_id+'</div>'
+					+'<div class="tableColumn tableCol-15" data-label="등록일">'+data.tenderList[i].t_creation_date+'</div>'
+					+'<div class="tableColumn tableCol-15" data-label="참여업체수">'+data.tenderList[i].company_count+'</div>'
+					+'<div class="tableColumn tableCol-15" data-label="마감기한">'+data.tenderList[i].d_day+'</div>'
+					+'</li>';
+			}
+			$("#tableListHeader").nextAll().remove();
+			$("#tableList").append(tenderList);
+			
+			if(data.criteria.prev){
+				page += '<li class="page-item"><a class="page-link" href="javascript:page('+(data.criteria.startPage-1)+');" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>'
+			}
+			for(var i=data.criteria.startPage; i<=data.criteria.endPage; i++){
+				page += '<li class="page-item"><a class="page-link" href="javascript:page('+i+');">'+i+'</a></li>'
+			}
+			if(data.criteria.next){
+				page += '<li class="page-item"><a class="page-link" href="javascript:page('+(data.criteria.endPage+1)+');" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>'
+			}
+			$(".pagination").empty();
+			$(".pagination").prepend(page);
+			$("html, body").animate({ scrollTop: 0 }, 1);
+		}
+	});
+}
+
+(function(){
+	$.ajax({
+		type:"post",
+		url:"/tCheck",
+		async:false,
+		dataType:"json",
+		success:function(data){
+			if(data.member_devision == 'no'){
+				var selectHtml='<select class="tenderSelect">'
+								+'<option value="tTender">전체 글 보기</option>'
+								+'<option value="mTender">내가 쓴 글 보기</option>'
+								+'</select>';
+				$("#tenderSelectDiv").append(selectHtml);
+				
+				var tenderWrite='<input type="submit" id="tenderWrite" class="tenderWriteBtn" name="tenderWrite" value="작성하기">';
+				$("#tenderWriteDiv").append(tenderWrite);
+			}
+		}
+	})
+}());
+
 $(document).ready(function(){
 	//리스트 클릭 시 세부 내용으로 이동
 	$(document).on('click','.tenderLiContent',function(){
