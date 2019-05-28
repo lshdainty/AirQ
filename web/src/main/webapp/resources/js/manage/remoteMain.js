7ee9bf8c57c52e155f9dc4d3e37fa60daf228dcb
Baesuhwi <44791399+Baesuhwi@users.noreply.github.com>
2019-05-28 17:52:09 +0900

    2019-05-28 원격 제품 등록

--- web/src/main/webapp/resources/js/manage/remoteMain.js
+++ web/src/main/webapp/resources/js/manage/remoteMain.js
@@ -6,4 +6,30 @@ $(document).ready(function(){
 		location.href="remoteRegist";
 	});
 	
+	// ON, OFF 클릭시
+	$(".onOff").click(function(){
+		alert($(this).is(":checked"));
+
+		$.ajax({
+			type : "POST",
+			contentType: "application/json;charset=UTF-8",
+			url : "/DataControl",
+			data : {
+				switch : $(this).is(":checked")
+			},
+			dataType : "json",
+			success : function(data){
+				console.log(data);
+				if($(this).is(":checked") == true){
+					alert("ON클릭");
+					
+				}else if($(this).is(":checked") == false){
+					alert("OFF클릭");
+				}
+				location.href="/test"
+			}
+			
+		});
+		
+	});
 });	// document
