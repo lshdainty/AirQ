bae9e079e8ab53ade5263d904072a1be22f404ef
CheonYohan <kero74@naver.com>
2019-06-04 21:53:25 +0900

    2019/06/04 compareMain

--- web/src/main/webapp/resources/js/connect/compareMain.js
+++ web/src/main/webapp/resources/js/connect/compareMain.js
@@ -1,11 +1,12 @@
-﻿$('.type_thumb').addClass('on');
-$('.type_thumb').click(function(){
+﻿$('.type_thumb').click(function(){
 	$('.type_list').removeClass('on');
 	$(this).addClass('on');
+	window.location.href="/compareMain_grid";
 });
 $('.type_list').click(function(){
 	$('.type_thumb').removeClass('on');
 	$(this).addClass('on');
+	window.location.href="/compareMain";
 });
 $.ajax({
 		type: "get",
