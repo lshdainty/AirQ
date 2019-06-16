64df27798c6495abf6c27bf04e689eeba0df3ce9
CheonYohan <kero74@naver.com>
2019-06-17 02:04:53 +0900

    2019/06/17 앱 내부모니터링
    슬라이드버그
    현재 공기질정보
    공기등급에 따른 표정변화

--- app/airq/www/resources/js/monitoring/monitoring.js
+++ app/airq/www/resources/js/monitoring/monitoring.js
@@ -1,8 +1,23 @@
-var ip = sessionStorage.getItem('IP_ADDRESS');
+// var ip = sessionStorage.getItem('IP_ADDRESS');
+
+var ip = "http://192.168.2.8";
+var member_id = JSON.parse(sessionStorage.getItem("user")).member_id;
 /* *************************** page init function ******************** */
+$.ajax({
+	type : "GET",
+	url : ip+"/m.inOldData",
+	data:{member_id:"kimminsu"},
+	dataType : "json",
+	success : function(data){
+		console.log(data);
+		$("#matterValue").text(data.matterValue);
+		$("#todayAvg").text(data.todayAvg);
+		$("#overValue").text(data.overValue);
+		inOldData(data.oldData);	//기본 30개값 가져오기
+	}
+});
 ajaxChart("신암동","pm10");
 ajaxArea("대구");
-liveChart();
 $("#areaName").val("신암동");
 $("#sido_code").val("대구").prop("selected", true);
 $("#sigoon_code").val("신암동").prop("selected", true);
@@ -164,99 +179,208 @@ $("#matter").val("pm10").prop("selected", true);
 
 /* ******************** */
 /* inside monitoring js */
+// live chart 시작
 
-function liveChart() {
+
+//live chart시작
+function inOldData(OldData) {
 	am4core.ready(function() {
 
 		// Themes begin
-		am4core.useTheme(am4themes_animated);
+		am4core.useTheme(am4themes_spiritedaway);
 		// Themes end
 
-		// Create chart instance
 		var chart = am4core.create("liveChart", am4charts.XYChart);
+		chart.hiddenState.properties.opacity = 0;
 
-		// Add data
-		chart.data = [ {
-			"country" : "1",
-			"visits" : 3025
-		}, {
-			"country" : "2",
-			"visits" : 1882
-		}, {
-			"country" : "3",
-			"visits" : 1809
-		}, {
-			"country" : "4",
-			"visits" : 1322
-		}, {
-			"country" : "5",
-			"visits" : 1122
-		}, {
-			"country" : "6",
-			"visits" : 1114
-		}, {
-			"country" : "7",
-			"visits" : 984
-		}, {
-			"country" : "8",
-			"visits" : 711
-		}, {
-			"country" : "9",
-			"visits" : 665
-		}, {
-			"country" : "10",
-			"visits" : 580
-		}, {
-			"country" : "11",
-			"visits" : 443
-		}, {
-			"country" : "12",
-			"visits" : 441
-		} ];
+		chart.padding(0, 0, 0, 0);
 
-		// Create axes
-		var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-		categoryAxis.dataFields.category = "country";
-		categoryAxis.renderer.grid.template.location = 0;
-		categoryAxis.renderer.minGridDistance = 1;
-		categoryAxis.renderer.labels.template.horizontalCenter = "middle";
-		categoryAxis.renderer.labels.template.verticalCenter = "middle";
-		categoryAxis.renderer.labels.template.rotation = 0;
-		categoryAxis.tooltip.disabled = true;
-		categoryAxis.renderer.minHeight = 0;
+		chart.zoomOutButton.disabled = true;
+
+		var data = [];
+		var value;
+		var i = 0;
+
+		for (i = 0; i < OldData.length; i++) {
+			value = OldData[i].VALUE;
+			data.push({
+				TIME : new Date(OldData[i].TIME),
+				VALUE : value
+			});
+		}
+		chart.data = data;
+
+		var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+		dateAxis.renderer.grid.template.location = 0;
+		dateAxis.renderer.minGridDistance = 30;
+		dateAxis.dateFormats.setKey("second", "ss");
+		dateAxis.periodChangeDateFormats.setKey("second", "[bold]h:mm a");
+		dateAxis.periodChangeDateFormats.setKey("minute", "[bold]h:mm a");
+		dateAxis.periodChangeDateFormats.setKey("hour", "[bold]h:mm a");
+		dateAxis.renderer.inside = true;
+		dateAxis.renderer.axisFills.template.disabled = true;
+		dateAxis.renderer.ticks.template.disabled = true;
 
 		var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-		valueAxis.renderer.minWidth = 0;
+		valueAxis.tooltip.disabled = true;
+		valueAxis.interpolationDuration = 500;
+		valueAxis.rangeChangeDuration = 500;
+		valueAxis.renderer.inside = true;
+		valueAxis.renderer.minLabelPosition = 0.05;
+		valueAxis.renderer.maxLabelPosition = 0.95;
+		valueAxis.renderer.axisFills.template.disabled = true;
+		valueAxis.renderer.ticks.template.disabled = true;
 
-		// Create series
-		var series = chart.series.push(new am4charts.ColumnSeries());
-		series.sequencedInterpolation = true;
-		series.dataFields.valueY = "visits";
-		series.dataFields.categoryX = "country";
-		series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
-		series.columns.template.strokeWidth = 0;
+		var series = chart.series.push(new am4charts.LineSeries());
+		series.dataFields.dateX = "TIME";
+		series.dataFields.valueY = "VALUE";
+		series.interpolationDuration = 500;
+		series.defaultState.transitionDuration = 0;
+		series.tensionX = 0.8;
 
-		series.tooltip.pointerOrientation = "vertical";
-
-		series.columns.template.column.cornerRadiusTopLeft = 10;
-		series.columns.template.column.cornerRadiusTopRight = 10;
-		series.columns.template.column.fillOpacity = 0.8;
-
-		// on hover, make corner radiuses bigger
-		var hoverState = series.columns.template.column.states.create("hover");
-		hoverState.properties.cornerRadiusTopLeft = 0;
-		hoverState.properties.cornerRadiusTopRight = 0;
-		hoverState.properties.fillOpacity = 1;
-
-		series.columns.template.adapter.add("fill", function(fill, target) {
-			return chart.colors.getIndex(target.dataItem.index);
+		chart.events.on("datavalidated", function() {
+			dateAxis.zoom({
+				start : 1 / 15,
+				end : 1.2
+			}, false, true);
 		});
 
-		// Cursor
-		chart.cursor = new am4charts.XYCursor();
+		dateAxis.interpolationDuration = 500;
+		dateAxis.rangeChangeDuration = 500;
 
-	}); // end am4core.ready()
-};
+		// 창이 내려갔을때 어떤 행동을 할것인가
+		// document.addEventListener("visibilitychange", function() {
+		// startInterval();
+		// if (document.hidden) {
+		// if (interval) {
+		// clearInterval(interval);
+		// }
+		// }
+		// else {
+		// startInterval();
+		// }
+		// }, false);
+
+		// add data
+		var interval;
+		function startInterval() {
+			interval = setInterval(function() {
+				$.ajax({
+					type : "GET",
+					url : ip+"/m.inNowData",
+					data: {member_id:'kimminsu'},
+					dataType : "json",
+					success : function(data) {
+						console.log(data);
+						$("#matterValue").text(data.matterValue);
+						$("#todayAvg").text(data.todayAvg);
+						$("#overValue").text(data.overValue);
+						var gradeText = "";
+						var gradeImage = "";
+						switch (data.grade) {
+						  case 1 :
+							  gradeText = "최고";
+							  gradeImage = "1";
+							  break;
+						  case 2 :
+							  gradeText = "좋음";
+							  gradeImage = "1";
+							  break;
+						  case 3 :
+							  gradeText = "양호";
+							  gradeImage = "2";
+							  break;
+						  case 4 :
+							  gradeText = "보통";
+							  gradeImage = "2";
+							  break;
+						  case 5 :
+							  gradeText = "나쁨";
+							  gradeImage = "3";
+							  break;
+						  case 6 :
+							  gradeText = "상당히 나쁨";
+							  gradeImage = "4";
+							  break;
+						  case 7 :
+							  gradeText = "매우 나쁨";
+							  gradeImage = "5";
+							  break;
+						  case 8 :
+							  gradeText = "최악";
+							  gradeImage = "6";
+							  break;
+						}
+						$("#face").attr('src',"/resources/images/face_"+gradeImage+".svg");
+						$(".info_grade__value").text(gradeText);
+						$(".info_grade__container").addClass("grade_"+data.grade);
+						chart.addData({
+							TIME : new Date(data.nowData[0].TIME),
+							VALUE : data.nowData[0].VALUE
+						}, 1);
+					}
+				});
+			}, 6000);
+		}
+
+		startInterval();
+
+		// all the below is optional, makes some fancy effects
+		// gradient fill of the series
+		series.fillOpacity = 1;
+		var gradient = new am4core.LinearGradient();
+		gradient.addColor(chart.colors.getIndex(0), 0.2);
+		gradient.addColor(chart.colors.getIndex(0), 0);
+		series.fill = gradient;
+
+		// this makes date axis labels to fade out
+		dateAxis.renderer.labels.template.adapter.add("fillOpacity", function(
+				fillOpacity, target) {
+			var dataItem = target.dataItem;
+			return dataItem.position;
+		})
+
+		// need to set this, otherwise fillOpacity is not changed and not set
+		dateAxis.events.on("validated", function() {
+			am4core.iter.each(dateAxis.renderer.labels.iterator(), function(
+					label) {
+				label.fillOpacity = label.fillOpacity;
+			})
+		})
+
+		// this makes date axis labels which are at equal minutes to be rotated
+		dateAxis.renderer.labels.template.adapter.add("rotation", function(
+				rotation, target) {
+			var dataItem = target.dataItem;
+			if (dataItem.date
+					&& dataItem.date.getTime() == am4core.time.round(
+							new Date(dataItem.date.getTime()), "minute")
+							.getTime()) {
+				target.verticalCenter = "middle";
+				target.horizontalCenter = "left";
+				return -90;
+			} else {
+				target.verticalCenter = "bottom";
+				target.horizontalCenter = "middle";
+				return 0;
+			}
+		})
+
+		// bullet at the front of the line
+		var bullet = series.createChild(am4charts.CircleBullet);
+		bullet.circle.radius = 5;
+		bullet.fillOpacity = 1;
+		bullet.fill = chart.colors.getIndex(0);
+		bullet.isMeasured = false;
+
+		series.events.on("validated", function() {
+			bullet.moveTo(series.dataItems.last.point);
+			bullet.validatePosition();
+		}); // end am4core.ready()
+	});
+}
+
+//live chart끝
 
 /* ******************** */
 
